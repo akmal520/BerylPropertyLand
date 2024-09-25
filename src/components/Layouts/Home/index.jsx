0bead6852980da18252210993578900f63320977
akmal520 <akmalbentar15@gmail.com>
2024-09-25 19:10:44 +0700

    remove property list in home page

--- src/components/Layouts/Home/index.jsx
+++ src/components/Layouts/Home/index.jsx
@@ -1,14 +1,11 @@
 import Navbar from '../Navbar/Navbar';
 import Hero from './Hero';
-import PropertyType from './PropertyType';
-import React from 'react';
 
 const HomeLayout = () => {
     return (
         <>
             <Navbar />
             <Hero />
-            <PropertyType />
         </>
     );
 };
