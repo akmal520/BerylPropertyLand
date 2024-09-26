27b6e86302e2bcf98b5f02ad8f05cf43440b60fa
akmal520 <akmalbentar15@gmail.com>
2024-09-26 21:26:15 +0700

    remove anoying

--- src/components/Layouts/Home/index.jsx
+++ src/components/Layouts/Home/index.jsx
@@ -1,9 +1,6 @@
 import Navbar from '../Navbar/Navbar';
 import Hero from './Hero';
-<<<<<<< HEAD
 import React from 'react';
-=======
->>>>>>> 3f1022a4546c153d7e1c49575b4e28a56d9448f5
 
 const HomeLayout = () => {
     return (
